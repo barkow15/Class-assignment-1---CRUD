637e75dbb5c877a936fed81499229934246dfd2f
ggustav1 <Dahlia01>
2019-02-28 11:36:53 +0100

    hej

--- src/main/java/com/company/DatabaseModel.java
+++ src/main/java/com/company/DatabaseModel.java
@@ -2,7 +2,6 @@ package com.company;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class DatabaseModel{
 
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         ConsoleColors cc = new ConsoleColors();
 
-        cc.printTxtGreen("text").printTxtYellow("Ok yay").printTxtUnderline("---- OMG ----").print(true);
 
         Menu menu = new Menu();
         menu.welcome();
